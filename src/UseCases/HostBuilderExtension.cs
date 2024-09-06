9ba1ddc5fe8c6ff6c8f3a5db34f5758466fb84f4
andriiantonyk <andrii@srgplus.com>
2024-09-06 20:01:24 +0300

    fix: add Mediatr service

---
+++ src/UseCases/HostBuilderExtension.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Telegram.TelegramOptions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UseCases.Abstractions;
+namespace UseCases;
+
+public static class HostBuilderExtension
+{
+    public static void ConfigureInfrastructureLayer(this IHostApplicationBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureMediatr();
+    }
+
+    private static void ConfigureMediatr(this IHostApplicationBuilder hostBuilder)
+    {
+        hostBuilder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ICommand).Assembly));
+    }
+}

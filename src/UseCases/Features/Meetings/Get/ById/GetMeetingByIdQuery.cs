bb3bbc85510ac9299eb28d10462be5c8ba2f9365
andriiantonyk <andrii@srgplus.com>
2024-09-07 14:45:33 +0300

    feat: add get meeting by id endpoint

---
+++ src/UseCases/Features/Meetings/Get/ById/GetMeetingByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using Domain.Primitives;
+using UseCases.Abstractions.Messaging;
+using UseCases.Features.Meetings.Get.Comon;
+namespace UseCases.Features.Meetings.Get.ById;
+
+public sealed record GetMeetingByIdQuery(string Id) : IQuery<MeetingResponse?>;
---
+++ src/UseCases/Features/Meetings/Get/ById/GetMeetingsByIdQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities.Meeting;
+using UseCases.Abstractions.Messaging;
+using UseCases.Features.Meetings.Get.Comon;
+namespace UseCases.Features.Meetings.Get.ById;
+
+public class GetMeetingsByIdQueryHandler(IMeetingRepository meetingRepository) : IQueryHandler<GetMeetingByIdQuery, MeetingResponse?>
+{
+    public async Task<MeetingResponse?> Handle(GetMeetingByIdQuery request, CancellationToken cancellationToken)
+    {
+        var meeting =  await meetingRepository.GetByIdAsync(new MeetingId(Ulid.Parse(request.Id)), cancellationToken);
+        
+        if (meeting is null) return null;
+        
+        return await MeetingResponse.FromDomain(meeting);
+    }
+}

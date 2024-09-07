79a31ef0fda7cb3787dcf3e77aed78340466110a
andriiantonyk <andrii@srgplus.com>
2024-09-07 15:59:48 +0300

    fix: remove stream from body

--- src/Api/Apis/Meetings/HandleZoomWebhook.cs
+++ src/Api/Apis/Meetings/HandleZoomWebhook.cs
@@ -1,8 +1,6 @@
-﻿using System.Threading.Tasks.Dataflow;
+﻿
 using Api.Abstractions;
 using Api.Extensions;
-using Microsoft.AspNetCore.Mvc;
-using Pathoschild.Http.Client.Internal;
 using UseCases.Features.Meetings.Webhook;
 using ZoomNet;
 namespace Api.Apis.Meetings;
@@ -19,12 +17,12 @@ public class HandleZoomWebhook : IEndpoint
                 StatusCodes.Status404NotFound);
     }
     
-    private static async Task<IResult> HandleWebhook([FromBody] Stream stream, [AsParameters] MeetingServices services)
+    private static async Task<IResult> HandleWebhook([AsParameters] MeetingServices services)
     {
-        var parser = new WebhookParser();
-        var @event = await parser.ParseEventWebhookAsync(stream).ConfigureAwait(false);
-
-        await services.Mediator.Send(new HandleZoomWebhookCommand(@event));
+        // var parser = new WebhookParser();
+        // var @event = await parser.ParseEventWebhookAsync(new Stream()).ConfigureAwait(false);
+        //
+        // await services.Mediator.Send(new HandleZoomWebhookCommand(@event));
         return Results.Ok();
     }
 }

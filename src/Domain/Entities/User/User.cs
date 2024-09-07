9fc2834f41ba5487e2c363d002f60b8004a9f011
andriiantonyk <andrii@srgplus.com>
2024-09-07 11:38:16 +0300

    fix: add migration

--- src/Domain/Entities/User/User.cs
+++ src/Domain/Entities/User/User.cs
@@ -23,5 +23,4 @@ public class User : Entity
             MessengerType = messengerType
         };
     }
-    
 }
---
+++ src/Infrastructure/Database/Migrations/20240907083715_Initial.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    username = table.Column<string>(type: "text", nullable: false),
+                    messenger_user_id = table.Column<string>(type: "text", nullable: false),
+                    chat_id = table.Column<string>(type: "text", nullable: false),
+                    messenger_type = table.Column<short>(type: "smallint", nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "meetings",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    creator_id = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    scheduled = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    url = table.Column<string>(type: "text", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_meetings", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_meetings_users_creator_id",
+                        column: x => x.creator_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "invitations",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    status = table.Column<short>(type: "smallint", nullable: false),
+                    meeting_id = table.Column<string>(type: "text", nullable: false),
+                    invitee_id = table.Column<string>(type: "text", nullable: false),
+                    inviter_id = table.Column<string>(type: "text", nullable: false),
+                    url = table.Column<string>(type: "text", nullable: false),
+                    created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_invitations", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_invitations_meetings_meeting_id",
+                        column: x => x.meeting_id,
+                        principalTable: "meetings",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_invitations_users_invitee_id",
+                        column: x => x.invitee_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_invitations_users_inviter_id",
+                        column: x => x.inviter_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_invitations_invitee_id",
+                table: "invitations",
+                column: "invitee_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_invitations_inviter_id",
+                table: "invitations",
+                column: "inviter_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_invitations_meeting_id",
+                table: "invitations",
+                column: "meeting_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_meetings_creator_id",
+                table: "meetings",
+                column: "creator_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "invitations");
+
+            migrationBuilder.DropTable(
+                name: "meetings");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
